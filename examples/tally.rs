da03f790f8384881750a98f696ef6b8a604c2aed
Simon <usersimon01@gmail.com>
2024-02-14 22:40:41 +0100

    Renamed the counter example -> tally
    Mostly just because saying 'counter example' was confusing

--- examples/tally.rs
+++ examples/tally.rs
@@ -2,11 +2,11 @@ use std::io;
 use ratatui::widgets::Paragraph;
 use tundra::prelude::*;
 
-struct Counter {
+struct Tally {
     value: u32, 
 }
 
-impl State for Counter {
+impl State for Tally {
     type Result<T> = T;
     type Out = u32;
     type Global = ();
@@ -19,7 +19,7 @@ impl State for Counter {
     fn input(mut self, key: KeyEvent, ctx: &mut Context) -> Signal<Self> {
         match key.code {
             KeyCode::Up    => self.value += 1, 
-            KeyCode::Tab   => self.value *= counter(ctx), 
+            KeyCode::Tab   => self.value *= tally(ctx), 
             KeyCode::Enter => return Signal::Return(self.value), 
             _ => (), 
         }
@@ -27,13 +27,13 @@ impl State for Counter {
     }
 }
 
-pub fn counter(ctx: &mut Context) -> u32 {
-    Counter{ value: 0 }.run(ctx)
+pub fn tally(ctx: &mut Context) -> u32 {
+    Tally{ value: 0 }.run(ctx)
 }
 
 fn main() -> io::Result<()> {
     let ctx = &mut Context::new()?;
-    let value = counter(ctx);
+    let value = tally(ctx);
     dialog::info(format!("You entered {value}! Why?"), &(), ctx);
 
     Ok(())

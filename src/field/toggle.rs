2da88da93c718945fe80e994705f236f0aca1705
Simon <usersimon01@gmail.com>
2024-10-11 20:43:35 +0200

    Refactor of Toggle to improve usability
    Improved rendered UX, made the value a BitBox, and added prefab error conditions to be used in field validtion

--- src/field/toggle.rs
+++ src/field/toggle.rs
@@ -1,20 +1,23 @@
-use std::{borrow::Cow, ops::RangeInclusive, iter};
-use ratatui::text::{Text, Line};
+use std::borrow::Cow;
+use bitvec::{bitbox, boxed::BitBox, slice::BitSlice};
+use ratatui::{
+    style::{Style, Stylize}, 
+    text::{Line, Span, Text}, 
+};
 use crate::prelude::*;
 use super::*;
 
 /// An [input field](super) for toggling a set of items on/off. 
 /// 
-/// The value is an array of booleans --- one for each item --- indicating whether the item corresponding to
-/// each index is toggled. See [`toggle::Builder`] for the methods available when constructing the field. 
+/// The value is a [`BitBox`] --- one bit for each item --- indicating whether the item corresponding to each
+/// index is toggled. See [`toggle::Builder`] for the methods available when constructing the field. 
 /// 
 /// 
 /// # Limiting the number of toggled items
 /// 
-/// The allowed range of the number items to be toggled at any given time can be customised with
-/// [`Toggle::range`] or [`Builder::range`]. E.g., a range of `2..=4` allows there to be up-to-and-including
-/// 4, but at least 2, toggled items. If the limit is reached and another item is toggled, the oldest toggled
-/// item is toggled off. The default range allows any number of items to be toggled. 
+/// Limits on the allowed number of toggled items can be introduced in [forms](dialog::form!) using field
+/// validation. To aid this, the following error conditions are defined in the [toggle] module: [`exactly`], 
+/// [`not_exactly`], [`less_than`], [`more_than`], [`outside_range`]. 
 /// 
 /// 
 /// # Key bindings
@@ -25,21 +28,12 @@ use super::*;
 pub struct Toggle {
     /// The user-visible name displayed by the input field. 
     pub name: Cow<'static, str>, 
-    /// The allowed range of the number of items to be toggled at any given time. 
-    pub range: RangeInclusive<usize>, 
     /// Index of the currently focused item. 
     focus: usize, 
     /// The user-visible names of the items that can be toggled. 
     items: Vec<Cow<'static, str>>, 
     /// Whether the item corresponding to each index is toggled. 
-    values: Vec<bool>, 
-    /// If the item corresponding to each index is toggled, contains the [`time`](Toggle::time) it was
-    /// toggled. It's split up in different fields like this to allow [`Toggle::values`] to be returned from
-    /// [`Field::value`]. 
-    log: Vec<Option<usize>>, 
-    /// The current "time". This is incremented each time an item is toggled, allowing its value to indicate
-    /// the age of a toggled item. 
-    time: usize, 
+    values: BitBox, 
 }
 
 impl Toggle {
@@ -58,98 +52,32 @@ impl Toggle {
             .into_iter()
             .map(Into::into)
             .collect();
-        debug_assert!(!self.items.is_empty());
+        assert!(!self.items.is_empty());
 
-        let min_toggled = self.range
-            .start()
-            .clone();
-        let values = iter::repeat(true)
-            .take(min_toggled)
-            .chain(iter::repeat(false))
-            .take(self.items.len());
-        self.set_values(values);
+        // set all values to 0
+        self.values = bitbox![0; self.items.len()];
     }
 
-    /// Sets the values of all items. Does not verify whether the values are valid according to the allowed
-    /// range (this is only an issue if a custom range is used). 
+    /// Sets the values at given indices. 
     /// 
     /// 
     /// # Panics
     /// 
-    /// When the number of values is not equal to the number of items. 
-    pub fn set_values(&mut self, values: impl IntoIterator<Item = bool>) {
-        let (values, log) = values
-            .into_iter()
-            .map(|b| (b, b.then_some(self.time)))
-            .unzip();
-        self.values = values;
-        self.log = log;
-
-        debug_assert!(self.values.len() == self.items.len());
+    /// When any given index is out of bounds. 
+    pub fn set_indices(&mut self, indices: impl IntoIterator<Item = usize>) {
+        for i in indices {
+            self.values.set(i, true);
+        }
     }
 
     /// Gets the names of the items that can be toggled. 
     pub fn items(&self) -> &[Cow<'static, str>] {
         &self.items
     }
-    
-    /// Toggles the item corresponding to an index on. 
-    /// 
-    /// 
-    /// # Returns
-    /// 
-    /// - `Ok(())` if the index could be toggled on. 
-    /// - `Err(())` if the [pop count](Toggle::pop_count) is equal to the
-    /// [upper limit](Toggle#limiting-the-number-of-toggled-items) and no items could be toggled off. 
-    pub fn toggle_on(&mut self, index: usize) -> Result<(), ()> {
-        if &self.pop_count() >= self.range.end() {
-            let oldest = self.log
-                .iter()
-                .enumerate()
-                .filter_map(|(i, time)| time.map(|time| (i, time)))
-                .min_by_key(|&(_, time)| time)
-                .map(|(i, _)| i);
-            let Some(oldest) = oldest else {
-                return Err(())
-            };
-            self.values[oldest] = false;
-            self.log[oldest] = None;
-        }
-        self.time += 1;
-        self.values[index] = true;
-        self.log[index] = Some(self.time);
-        Ok(())
-    }
-
-    /// Toggles the item corresponding to an index off. 
-    /// 
-    /// 
-    /// # Returns
-    /// 
-    /// - `Ok(())` if the index could be toggled off. 
-    /// - `Err(())` if the [pop count](Toggle::pop_count) is equal to the
-    /// [lower limit](Toggle#limiting-the-number-of-toggled-items). 
-    pub fn toggle_off(&mut self, index: usize) -> Result<(), ()> {
-        if &self.pop_count() > self.range.start() {
-            self.values[index] = false;
-            self.log[index] = None;
-            Ok(())
-        } else {
-            Err(())
-        }
-    }
-
-    /// The current number of toggled items. 
-    pub fn pop_count(&self) -> usize {
-        self.values
-            .iter()
-            .filter(|&&t| t)
-            .count()
-    }
 }
 
 impl Field for Toggle {
-    type Value = Vec<bool>;
+    type Value = BitBox;
     type Builder = Builder;
 
     fn name(&self) -> &str {
@@ -173,33 +101,35 @@ impl Field for Toggle {
 
             // toggle focused item on/off
             _ => {
-                let result = match self.values[self.focus] {
-                    true  => self.toggle_off(self.focus), 
-                    false => self.toggle_on(self.focus), 
-                };
-                match result {
-                    Ok(_) => InputResult::Updated, 
-                    Err(_) => InputResult::Ignored, 
-                }
+                let mut bit = self.values
+                    .get_mut(self.focus)
+                    .expect("Focus is in range");
+                *bit = !*bit;
+                InputResult::Updated
             }
         }
     }
 
     fn format(&self, focused: bool) -> Text {
-        let format_item = |i, item, toggled| {
-            let symbol = match toggled {
-                true  => '◼', 
-                false => ' ', 
-            };
-            match focused && i == self.focus {
-                true => format!("[{symbol}] {item}"), 
-                false => format!("({symbol}) {item}"), 
-            }
-        };
-        iter::zip(self.items.iter(), self.values.iter())
+        std::iter::zip(self.items.iter(), self.values.iter())
             .enumerate()
-            .map(|(i, (item, &value))| format_item(i, item, value))
-            .map(Line::from)
+            .map(|(i, (item, value))| {
+                let value = *value;
+                let symbol = match value {
+                    true => "✓", 
+                    false => " ", 
+                };
+                let style = Style::new().bold();
+                match focused && i == self.focus {
+                    true => Line::from(vec![
+                        Span::styled("<", style), 
+                        Span::from(symbol), 
+                        Span::styled("> ", style), 
+                        Span::from(item.as_ref()), 
+                    ]), 
+                    false => Line::from(format!("({symbol}) {item}")), 
+                }
+            })
             .collect::<Vec<_>>()
             .into()
     }
@@ -213,6 +143,42 @@ impl Field for Toggle {
     }
 }
 
+/// Check whether number of toggled items is exactly `N`. 
+/// 
+/// Defined for use in field validation for [`Toggle`]. 
+pub fn exactly<const N: usize>(bits: &BitSlice) -> bool {
+    bits.count_ones() == N
+}
+
+/// Check whether number of toggled items is not exactly `N`. 
+/// 
+/// Defined for use in field validation for [`Toggle`]. 
+pub fn not_exactly<const N: usize>(bits: &BitSlice) -> bool {
+    bits.count_ones() != N
+}
+
+/// Check whether number of toggled items is less than `N`. 
+/// 
+/// Defined for use in field validation for [`Toggle`]. 
+pub fn less_than<const N: usize>(bits: &BitSlice) -> bool {
+    bits.count_ones() < N
+}
+
+/// Check whether number of toggled items is more than `N`. 
+/// 
+/// Defined for use in field validation for [`Toggle`]. 
+pub fn more_than<const N: usize>(bits: &BitSlice) -> bool {
+    bits.count_ones() > N   
+}
+
+/// Check whether number of toggled items is less than `LOW` or more than `HIGH_INCLUSIVE`. 
+/// 
+/// Defined for use in field validation for [`Toggle`]. 
+pub fn outside_range<const LOW: usize, const HIGH_INCLUSIVE: usize>(bits: &BitSlice) -> bool {
+    let count = bits.count_ones();
+    count < LOW || count > HIGH_INCLUSIVE
+}
+
 /// Constructs a [`Toggle`]. 
 /// 
 /// This is used by the [form macro](crate::dialog::form!) when instantiating [toggles](Toggle), but may be
@@ -223,13 +189,10 @@ pub struct Builder<const NAME: bool = false, const ITEMS: bool = false>(Toggle);
 impl Default for Builder {
     fn default() -> Self {
         Self(Toggle {
-            name: Default::default(), 
-            range: 0..=usize::MAX, 
+            name: Cow::default(), 
             focus: 0, 
-            items: Default::default(), 
-            values: Default::default(), 
-            log: Default::default(), 
-            time: 0, 
+            items: Vec::default(), 
+            values: BitBox::default(), 
         })
     }
 }
@@ -257,34 +220,15 @@ impl<const NAME: bool, const ITEMS: bool> Builder<NAME, ITEMS> {
 }
 
 impl<const NAME: bool> Builder<NAME, true> {
-    /// The initial values of all items. 
+    /// Sets the values at given indices. 
     /// 
     /// 
     /// # Panics
     /// 
-    /// When the number of values is not equal to the number of items. 
-    pub fn values(mut self, values: impl IntoIterator<Item = bool>) -> Self {
-        self.0.set_values(values);
+    /// When any given index is out of bounds. 
+    pub fn set(mut self, indices: impl IntoIterator<Item = usize>) -> Self {
+        self.0.set_indices(indices);
         Builder(self.0)
-    }
-
-    /// The allowed range of toggled values. See the
-    /// [type-level](Toggle#limiting-the-number-of-toggled-items) documentation for more information. Ensures
-    /// that at least `range.start()` items are toggled. 
-    pub fn range(mut self, range: RangeInclusive<usize>) -> Self {
-        let min = range
-            .start()
-            .clone();
-        let difference = min.saturating_sub(self.0.pop_count());
-        let free = iter::zip(self.0.values.iter_mut(), self.0.log.iter_mut())
-            .filter(|(&mut b, _)| !b)
-            .take(difference);
-
-        for (value, log) in free {
-            *value = true;
-            *log = Some(0)
-        }
-        Builder(Toggle{ range, ..self.0 })
     }
 }
 
